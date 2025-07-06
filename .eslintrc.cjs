391b4af4440c9b7665d9c653d9884a1f38f86378
wanderlei jose bolzan <wbolzanjp@gmail.com>
2025-07-05 21:16:49 -0300

    Fourth pull request - Task: REACT_TODO-APP-LOADING-TODOS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  root: true,
+  root: true, // Garante que esta é a configuração raiz
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
@@ -11,7 +11,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-
+    // Adicione 'plugin:prettier/recommended' se você usa Prettier
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -19,13 +19,12 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-
-    project: './tsconfig.json',
+    // REMOVIDO: project: './tsconfig.json', // Esta linha foi removida do nível superior
     tsconfigRootDir: __dirname,
   },
   settings: {
     react: {
-      version: 'detect',
+      version: 'detect', // Detecta automaticamente a versão do React
     },
   },
   env: {
@@ -34,31 +33,40 @@ module.exports = {
     node: true,
   },
   rules: {
-
+    // Adicione suas regras ESLint específicas aqui
+    "react/prop-types": "off", // Desativa a validação de propTypes, pois TypeScript já lida com isso
+    "react/react-in-jsx-scope": "off" // Desativa a necessidade de importar React explicitamente para JSX (para React 17+)
   },
   overrides: [
     {
-
+      // Aplica estas regras e configurações a arquivos dentro da pasta cypress e ao cypress.config.ts
       files: ['cypress/**/*.ts', 'cypress.config.ts'],
       parserOptions: {
-
-        project: './tsconfig.cypress.json',
+        // Usa o tsconfig.cypress.json para esses arquivos
+        project: './tsconfig.cypress.json', // Aponta para o tsconfig.cypress.json
         tsconfigRootDir: __dirname,
       },
       extends: [
-        'plugin:cypress/recommended',
+        'plugin:cypress/recommended', // Adiciona regras específicas do Cypress
         'plugin:@typescript-eslint/recommended',
       ],
       rules: {
-
         '@typescript-eslint/no-unused-expressions': 'off',
         'cypress/no-unnecessary-waiting': 'warn',
       },
       env: {
-        'cypress/globals': true,
-        node: true,
-        browser: false
+        'cypress/globals': true, // Expõe as variáveis globais do Cypress (cy, expect, etc.)
+        node: true, // O arquivo de configuração do Cypress é executado em ambiente Node.js
+        browser: false // Não há globais de navegador para o arquivo de configuração
       },
     },
+    // Adicione uma nova entrada para arquivos de código-fonte da aplicação se precisar de linting com tipagem
+    {
+      files: ['src/**/*.{ts,tsx}'], // Aplica a arquivos TypeScript/TSX em src/
+      parserOptions: {
+        project: './tsconfig.json', // Aponta para o tsconfig.json principal para o código da aplicação
+        tsconfigRootDir: __dirname,
+      },
+    }
   ],
 };
